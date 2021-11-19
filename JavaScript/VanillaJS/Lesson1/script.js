c3b95025a5d469649409028a4e2fa83ee7b7b2aa
Hasan Bakhtiar <hasan.doctype@gmail.com>
2021-11-19 20:23:06 +0400

    js l1

---
+++ JavaScript/VanillaJS/Lesson1/script.js
@@ -0,0 +1,34 @@
+//one line  comment
+
+/*more 
+line
+comment */
+
+// alert('Bye'); //new 
+// window.alert("Hello1"); //gloabl alert
+
+
+// debugger;
+
+// console.log("Hello");
+// console.log("Hello");
+// console.log("Hello");
+// console.log("Hello");
+// console.log("Hello");
+// console.log("Hello");
+// console.log("Hello");
+// console.log("Hello");
+// console.log("Hello");
+// console.log("Hello");
+// console.log("Hello");
+// console.log("Hello");
+// console.log("Hello");
+// console.log("Hello");
+// console.log("Hello");
+// console.log("Hello");
+// console.log("Hello");
+// console.log("Hello");
+
+
+
+
