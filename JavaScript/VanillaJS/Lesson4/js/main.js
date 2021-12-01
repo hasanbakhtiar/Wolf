429942195a59801aaaff3905b91b32329b5243c1
Hasan Bakhtiar <hasan.doctype@gmail.com>
2021-12-01 20:55:52 +0400

    JS Lesson4

---
+++ JavaScript/VanillaJS/Lesson4/js/main.js
@@ -0,0 +1,62 @@
+// switch - şərt
+// case - şərtin qarşılığı
+// default - şərtin ödənmədiyi
+// break - şərtin bağlanıldığı hissə 
+
+// let a = true;
+// switch (a) {
+//     case (true||false):
+//         console.log("a is 5");
+//         break;
+        
+//     case "Hello":
+//         console.log('a is Hello');    
+//         break;
+
+//     default:
+//         console.log("this is false");
+//         break;
+// }
+
+
+
+// try {
+//     throw("Everything Ok");
+
+//     documeasadkhsahdgajnt.write("Hello");
+//     documeasadkhsahdgajnt.write("Hello1");
+// } catch (err) {
+//     document.write(err);
+//     // document.write("My Error: "+err);
+// }finally{
+//     // document.write("<br>Im always runing...");
+//     document.write("<br>When ur have problems contact with us <a href='https://google.com'>Contact Page</a>");
+// }
+
+
+// Loops
+// let infoData = 1;
+
+// while (infoData <= 100) {
+//     console.log(infoData);
+//     infoData*=2;
+// }
+
+// let infoData = 1;
+
+// do {
+//     console.log(infoData);
+//     infoData++;
+// } while (infoData>=100);
+
+// for(let infoData = 1; infoData<=100; infoData++){
+//     document.write(infoData+"<br>");
+
+// }
+
+let infoBox = ["Table", "Board", "Pen"];
+
+for(ahjdasd in infoBox){
+    console.log(infoBox[ahjdasd]);
+}
+console.log(infoBox);
