8fa230aae5eb690f8fa2532bf2394f98ec846edc
Hasan Bakhtiar <hasan.doctype@gmail.com>
2021-12-22 20:57:14 +0400

    jquery lesson1

---
+++ JavaScript/JQuery/Lesson1/js/main.js
@@ -0,0 +1,27 @@
+// $(function(){
+//     $(".btn")//nə ilə
+//     .click(function(){ //necə
+//         $('p')//nəyi
+//         .hide();// etsin
+//     })
+// })
+
+
+$(function(){
+    $("button.btn").click(function(){
+    //   $("p").fadeOut(1000);
+    //   $("p").fadeIn("slow");
+    //   $("p").fadeToggle(2000);
+    //   $("p").fadeTo(2000,0.1);
+    //   $("p:odd").hide(1000);
+    //   $("p:even").hide(1000);
+    //   $(".con>*").hide(1000);
+    //   $("input:text").hide(1000);
+    });
+
+
+    // $(".btn").dblclick(function(){
+    //     $(".con>*").show(1000);
+    //   });
+
+  });
