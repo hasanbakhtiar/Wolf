676a662675550ac9a28237bab645faa59352fff1
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-01-19 20:59:10 +0400

    webpack

---
+++ JavaScript/ReactJS/lesson2-webpack/src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './sass/style.scss';
+const root = document.querySelector('#root');
+
+const App =()=>{return(<h1>HEllo</h1>)};
+
+ReactDOM.render(<App/>,root);
+
